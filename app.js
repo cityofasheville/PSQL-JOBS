59017b22f350dbced51ab3e248ae6647d690888f
sde <sde@coa-gis-jobs1.asheville.local>
2015-03-20 15:07:21 -0400

    framework for node based sql job engine

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 var yaml = require('yamljs');
 var pg = require('pg');
+var fs = require('fs');
 
 // Load yaml file using YAML.load 
 nativeObject = yaml.load('config/config.yml');
 
-//console.log(JSON.stringify(nativeObject.databases.length));
 obj = nativeObject.databases
 for (var prop in obj) {
-  //host = obj['host'];
-  //database = obj['databbase'];
-  //console.log(host + database)
- //console.log(obj[prop]);
- childobj =  obj[prop];
- console.log(childobj.host + '-' + childobj.database);
- 
- //for (var childprop in childobj) {
-  //console.log(childprop);
-  //console.log(childobj[childprop]); 
- //  dbObj =  childobj;
- //  console.log(dbObj.host + '-' + dbObj.database);
- //  var conString = "postgres://"+dbObj.user+":"+dbObj.pasword+"@"+dbObj.host+":5432/"+dbObj.database
- //}
- //console.log('---------------');
-} 
+  aQuery = obj[prop].preparedStatments[0];
 
+  var client = new pg.Client(obj[prop])
+  client.on('drain', client.end.bind(client)) 
+  client.connect(function(err, client, done){
+    if(err) {
+      // this should print "error: canceling statement due to user request"
+      console.error("%s", err);
+    }
+  });
+  
+  var first = client.query(aQuery, function(err, result) {
+    if(err) {
+      // this should print "error: canceling statement due to user request"
+      console.error("%s", err);
+    };
+    //console.log(result);
+  });
+  first.on('row', function(row) {
+     console.log(row.address);
+  });
+};
