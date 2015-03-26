06c61c50ca6bba699cd17696d67f990c9671df9d
sde <sde@coa-gis-jobs1.asheville.local>
2015-03-26 08:48:56 -0400

    Remove Copy-n-Paste duplication

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ for (prop in obj) {
         });
 
         query.on('end', function (result) {
-            //fired once and only once, after the last row has been returned and after all 'row' events are emitted
+            //fired once and only once, after the last row has been returned and aftera all 'row' events are emitted
             //in this example, the 'rows' array now contains an ordered set of all the rows which we received from postgres
             'use strict';
             console.log(result.rowCount + ' rows were received');
